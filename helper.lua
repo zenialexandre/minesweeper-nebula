518d2d7131ec133a8004e42661bdb63190fe6b1f
zenialexandre <zenialexandre123@gmail.com>
2025-11-06 19:37:37 -0300

    feature(grid, gameplay): working on the gameplay loop

--- helper.lua
+++ helper.lua
@@ -1,10 +1,29 @@
-function enum(table)
-    return setmetatable(table, {
-        __index = function(_, key)
-            error("Invalid enumerator key: " .. tostring(key))
-        end,
-        __newindex = function()
-            error("Immutable enumarator.")
+function enum(input_table, is_immutable)
+    local meta_table = {}
+
+    function meta_table:random()
+        local keys = {}
+
+        for key in pairs(input_table) do
+            table.insert(keys, key)
         end
-    })
+
+        local random_key = keys[math.random(1, #keys)]
+        return self[random_key]
+    end
+
+    meta_table.__index = function(_, key)
+        if meta_table[key] ~= nil then
+            return meta_table[key]
+        end
+        error("Invalid enumerator key: " .. tostring(key))
+    end
+
+    if (is_immutable) then
+        meta_table.__newindex = function()
+            error("Immutable enumerator")
+        end
+    end
+
+    return setmetatable(input_table, meta_table)
 end
--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+math.randomseed(os.time())
 require "helper"
 
 _G.GameObserver = {
@@ -8,14 +9,14 @@ _G.GameObserver = {
     },
     grid_tracker = {
         available_grid_cells = 81,
-        available_numerical_grid_cells = 0,
-        available_mine_grid_cells = 0
+        available_numerical_grid_cells = 71,
+        available_mine_grid_cells = 10
     }
 }
 
 _G.GridType = enum {
-    NUMERICAL = 0,
-    MINE = 1
+    NUMERICAL = 1,
+    MINE = 2
 }
 
 function nebula.setup()
@@ -26,13 +27,14 @@ function nebula.setup()
     Position = nebula.ecs.component("Position")
     Quad = nebula.ecs.component("Quad")
     Color = nebula.ecs.component("Color")
-    GridMatrix = nebula.ecs.component("GridMatrix", {internal_matrix = {}})
-    GridCell = nebula.ecs.component("GridCell", {type = GridType.NUMERICAL})
+    GridMatrix = nebula.ecs.component("GridMatrix", {value = {}})
+    GridCell = nebula.ecs.component("GridCell", {value = GridType.NUMERICAL})
 
-    local mutable_position = Position({x = 6, y = 10});
+    local mutable_position = Position({x = 6, y = 10})
     local matrix = {}
-    local number_of_rows = 9;
-    local number_of_columns = 9;
+    local number_of_mines = 0
+    local number_of_rows = 9
+    local number_of_columns = 9
     local grid = nebula.ecs.spawn()
     --local score = nebula.ecs.spawn()
     --local timer = nebula.ecs.spawn()
@@ -48,6 +50,15 @@ function nebula.setup()
         for j = 1, number_of_columns do
             local grid_cell = nebula.ecs.spawn()
             local position = mutable_position
+            local grid_cell_type = GridType:random()
+
+            if (number_of_mines == 10) then
+                grid_cell_type = GridType.NUMERICAL
+            end
+
+            if (GridType.MINE == grid_cell_type) then
+                number_of_mines = number_of_mines + 1
+            end
 
             if (j ~= 1) then
                 position = Position({x = mutable_position.x + 56, y = mutable_position.y})
@@ -59,17 +70,37 @@ function nebula.setup()
                 position,
                 Quad({width = 40, height = 40}),
                 Color({r = 0.238, g = 0.239, b = 0.237}),
-                GridCell({type = GridType.NUMERICAL})
+                GridCell({value = grid_cell_type})
             )
             matrix[i][j] = grid_cell
         end
     end
 
-    nebula.ecs.addComponent(grid, GridMatrix({internal_matrix = matrix}))
+    nebula.ecs.addComponent(grid, GridMatrix({value = matrix}))
 end
 
-function nebula.update(dt)
+function nebula.update(delta)
+    if (nebula.mouse.isPressed("left")) then
+        local entities = nebula.ecs.getEntitiesWith(GridCell)
+        local mouse_x = nebula.mouse.getX()
+        local mouse_y = nebula.mouse.getY()
 
+        for entity in pairs(entities) do
+            local quad = nebula.ecs.getComponent(entity, Quad)
+            local position = nebula.ecs.getComponent(entity, Position)
+            local grid_cell = nebula.ecs.getComponent(entity, GridCell)
+
+            local quadrant_x = position.x + quad.width
+            local quadrant_y = position.y + quad.height
+
+            local is_mouse_in_quadrant_x = mouse_x <= quadrant_x and mouse_x >= quadrant_x
+            local is_mouse_in_quadrant_y = mouse_y <= quadrant_y and mouse_y >= quadrant_y
+
+            if (GridType.MINE == grid_cell.value and (is_mouse_in_quadrant_x and is_mouse_in_quadrant_y)) then
+                print("ops!! booom")
+            end
+        end
+    end
 end
 
 function nebula.draw()
